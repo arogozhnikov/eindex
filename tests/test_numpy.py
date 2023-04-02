d0a4099450bd0e24da726bcc062fc7631548c86a
Alex Rogozhnikov <iamfullofspam@gmail.com>
2023-04-01 21:18:26 -0700

    tests: verification of reductions in numpy

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -1,9 +1,9 @@
-from typing import TypeVar
+from typing import TypeVar, List, Tuple, Any
 
 import numpy as np
 
 from verboseindex._core import CompositionDecomposition
-from verboseindex.numpy import argmax, argmin, argsort, einindex, gather, gather_scatter, numpy_ixp, scatter
+from verboseindex.numpy import argmax, argmin, argsort, einindex, gather, gather_scatter, _numpy_ixp, scatter
 
 from .utils import (
     _enum_1d,
@@ -37,23 +37,20 @@ def pseudo_random_tensor(_, shape):
     return randint(size=shape, low=0, high=10000)
 
 
-# TODO tests for inf, +inf, -inf, nan?
-
-
 def test_composition_and_decomposition():
     x = range_of_shape(2, 3, 5, 7, xp=np)
     comp = CompositionDecomposition(
         decomposed_shape=["a", "b", "c", "d"],
         composed_shape=[["a", "b"], ["c", "d"]],
     )
-    x_composed = comp.compose_ixp(numpy_ixp, x, known_axes_lengths={})
+    x_composed = comp.compose_ixp(_numpy_ixp, x, known_axes_lengths={})
     assert x_composed.shape == (2 * 3, 5 * 7)
     assert np.all(x_composed == np.reshape(x, (2 * 3, 5 * 7)))
 
     y = CompositionDecomposition(
         decomposed_shape=["a", "b", "c", "d"],
         composed_shape=[["a", "b"], [], ["c", "d"], []],
-    ).compose_ixp(numpy_ixp, x, {})
+    ).compose_ixp(_numpy_ixp, x, {})
     assert y.shape == (2 * 3, 1, 5 * 7, 1)
     assert np.all(np.reshape(x, (-1,)) == np.reshape(y, (-1,)))
 
@@ -64,12 +61,12 @@ def test_composition_and_decomposition():
     x = range_of_shape(2, 3, 5, 7, 3, xp=np)
 
     axes = {}
-    y = comp.compose_ixp(numpy_ixp, x, axes)
+    y = comp.compose_ixp(_numpy_ixp, x, axes)
     assert y.shape == (5 * 7, 3, 2 * 3)
     y_manual = np.reshape(np.transpose(x, (2, 3, 1, 0, 4)), y.shape)
 
     assert np.all(y == y_manual)
-    x2 = comp.decompose_ixp(numpy_ixp, y, axes)
+    x2 = comp.decompose_ixp(_numpy_ixp, y, axes)
     assert np.all(x == x2)
 
 
@@ -85,9 +82,6 @@ def test_simple_indexing():
     y = einindex("j <- j i, [i] j", np.transpose(arr, (1, 0)), [ind])
     for j, i in _enum_1d(ind):
         assert arr[i, j] == y[j]
-
-
-test_simple_indexing()
 
 
 def test_multidimensional_indexing():
@@ -134,7 +128,8 @@ def test_reverse_indexing():
     W = 9
 
     arr_gtbc = (
-        +arange_at(np, 4, 0, G) * 1000
+        0
+        + arange_at(np, 4, 0, G) * 1000
         + arange_at(np, 4, 1, T) * 100
         + arange_at(np, 4, 2, B) * 10
         + arange_at(np, 4, 3, C) * 1
@@ -170,7 +165,7 @@ def test_argmax_straight():
     assert x[a, b, c, d] == np.max(x)
     cad = argmax(x, "a b c d -> [c, a, d] b")
     comp = CompositionDecomposition(composed_shape=[["c", "a", "d"], ["b"]], decomposed_shape=["a", "b", "c", "d"])
-    reference = np.argmax(comp.compose_ixp(numpy_ixp, x, {}), axis=0)
+    reference = np.argmax(comp.compose_ixp(_numpy_ixp, x, {}), axis=0)
     assert np.all(reference == compose_index(cad, [C, A, D]))
 
 
@@ -244,20 +239,88 @@ def test_index():
 
     array = generate_array(np, "a b c d", sizes=sizes)
     indexer = generate_indexer(np, "[a, c] d f g", sizes=sizes)
-    result = einindex("g f d b <- a b c d, [a, c] d f g", array, indexer)
+    result1 = einindex("g f d b <- a b c d, [a, c] d f g", array, indexer)
     result2 = gather("g f d b <- a b c d, [a, c] d f g", array, indexer)
-    assert np.allclose(result, result2)
-    indexer_as_dict = enumerate_indexer(numpy_ixp, "[a, c] d f g", indexer=indexer, sizes=sizes)
+    assert np.allclose(result1, result2)
+    indexer_as_dict = enumerate_indexer(_numpy_ixp, "[a, c] d f g", indexer=indexer, sizes=sizes)
 
     for b in range(sizes["b"]):
         flat_index_arr = to_flat_index("a b c d", {**indexer_as_dict, "b": b}, sizes=sizes)
         flat_index_result = to_flat_index("g f d b", {**indexer_as_dict, "b": b}, sizes=sizes)
 
         array_flat = flatten(np, array)
-        result_flat = flatten(np, result)
+        result_flat = flatten(np, result1)
 
         for i, j in zip(flat_index_arr, flat_index_result, strict=True):
             assert array_flat[i] == result_flat[j]
+
+
+def list_aggname_aggfunc_default_value() -> List[Tuple[str, Any, float]]:
+    return [
+        ("sum", lambda a, b: a + b, 0.0),
+        ("min", min, np.inf),
+        ("max", max, -np.inf),
+    ]
+
+
+def test_gather():
+    sizes = dict(
+        a=2,
+        b=3,
+        c=5,
+        d=7,
+        i1=3,
+        i2=5,
+        r=3,
+    )
+
+    final_pattern = "b c d"
+    array_pattern = "b i1 i2 d r"
+    index_pattern = "[i1, i2] c b a r"
+    full_pattern = f"{final_pattern} <- {array_pattern}, {index_pattern}"
+    array = generate_array(np, array_pattern=array_pattern, sizes=sizes)
+    indexer = generate_indexer(np, index_pattern, sizes=sizes)
+    result_gather = gather(full_pattern, array, indexer, aggregation="sum")
+    indexer_as_dict = enumerate_indexer(_numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
+
+    array_flat = flatten(np, array)
+    result_flat = flatten(np, result_gather)
+
+    for d in range(sizes["d"]):
+        flat_index_array = to_flat_index(array_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
+        flat_index_final = to_flat_index(final_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
+
+        for ia, ir in zip(flat_index_array, flat_index_final, strict=True):
+            result_flat[ir] -= array_flat[ia]
+
+    assert np.max(abs(result_flat)) == 0
+
+    # checking different aggregations
+
+    for agg_name, agg_func, default_value in [
+        ("sum", lambda a, b: a + b, 0.0),
+        ("min", min, np.inf),
+        ("max", max, -np.inf),
+    ]:
+        result_gather = gather(full_pattern, array, indexer, aggregation=agg_name).reshape(-1)
+        result_ref = np.full(shape=[sizes[d] for d in final_pattern.split()], fill_value=default_value).reshape(-1)
+        for d in range(sizes["d"]):
+            flat_index_array = to_flat_index(array_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
+            flat_index_final = to_flat_index(final_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
+
+            for ia, ir in zip(flat_index_array, flat_index_final, strict=True):
+                result_ref[ir] = agg_func(array_flat[ia], result_ref[ir])
+        assert np.allclose(result_ref, result_gather)
+
+    # checking mean aggregation on constant tensor
+    result_mean_const = gather(full_pattern, array * 0 + 3.0, indexer, aggregation="mean")
+    assert np.allclose(result_mean_const, 3.0)
+
+    # testing that ratio is constant, as number of elements averaged is the same for every result entry
+    result_mean = gather(full_pattern, array.clip(0) + 1.0, indexer, aggregation="mean")
+    result__sum = gather(full_pattern, array.clip(0) + 1.0, indexer, aggregation="sum")
+    ratio = result_mean / result__sum
+    assert ratio.min() * 0.99 < ratio.max() < ratio.min() * 1.01
 
 
 def test_scatter():
@@ -274,12 +337,11 @@ def test_scatter():
     array_pattern = "b c d"
     index_pattern = "[f, h] c b e"
     final_pattern = "b f h d e"
+    full_pattern: str = f"{final_pattern} <- {array_pattern}, {index_pattern}"
     array = generate_array(np, array_pattern=array_pattern, sizes=sizes)
     indexer = generate_indexer(np, index_pattern, sizes=sizes)
-    result = scatter(
-        f"{final_pattern} <- {array_pattern}, {index_pattern} ", array, indexer, f=sizes["f"], h=sizes["h"]
-    )
-    indexer_as_dict = enumerate_indexer(numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
+    result = scatter(full_pattern, array, indexer, f=sizes["f"], h=sizes["h"])
+    indexer_as_dict = enumerate_indexer(_numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
 
     array_flat = flatten(np, array)
     result_flat = flatten(np, result)
@@ -293,37 +355,27 @@ def test_scatter():
 
     assert np.max(abs(result_flat)) == 0
 
+    # check different aggregations
+    for agg_name, agg_func, default_value in list_aggname_aggfunc_default_value():
+        result_scatter = scatter(
+            full_pattern, array, indexer, aggregation=agg_name, f=sizes["f"], h=sizes["h"]
+        ).reshape(-1)
+        result_ref = np.full(
+            shape=[sizes[d] for d in final_pattern.split()], fill_value=default_value, dtype=array.dtype
+        ).reshape(-1)
+        for d in range(sizes["d"]):
+            flat_index_array = to_flat_index(array_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
+            flat_index_final = to_flat_index(final_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
 
-def test_gather():
-    sizes = dict(
-        a=2,
-        b=3,
-        c=5,
-        d=7,
-        f=3,
-        h=5,
-        r=3,
-    )
+            for ia, ir in zip(flat_index_array, flat_index_final, strict=True):
+                result_ref[ir] = agg_func(array_flat[ia], result_ref[ir])
+        assert np.allclose(result_ref, result_scatter)
 
-    final_pattern = "b c d"
-    array_pattern = "b f h d r"
-    index_pattern = "[f, h] c b a r"
-    array = generate_array(np, array_pattern=array_pattern, sizes=sizes)
-    indexer = generate_indexer(np, index_pattern, sizes=sizes)
-    result = gather(f"{final_pattern} <- {array_pattern}, {index_pattern} ", array, indexer)
-    indexer_as_dict = enumerate_indexer(numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
-
-    array_flat = flatten(np, array)
-    result_flat = flatten(np, result)
-
-    for d in range(sizes["d"]):
-        flat_index_array = to_flat_index(array_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
-        flat_index_final = to_flat_index(final_pattern, {**indexer_as_dict, "d": d}, sizes=sizes)
-
-        for ia, ir in zip(flat_index_array, flat_index_final, strict=True):
-            result_flat[ir] -= array_flat[ia]
-
-    assert np.max(abs(result_flat)) == 0
+    # testing aggregation on constant
+    result = scatter(full_pattern, array * 0 + 3.0, indexer, aggregation="mean", f=sizes["f"], h=sizes["h"])
+    result_sum = scatter(full_pattern, array * 0 + 1.0, indexer, aggregation="sum", f=sizes["f"], h=sizes["h"])
+    assert np.allclose(result[result_sum > 0], 3.0)
+    assert np.all(np.isnan(result[result_sum == 0]))
 
 
 def test_gather_scatter():
@@ -331,19 +383,20 @@ def test_gather_scatter():
         b=3,
         c=5,
         r=3,
+        f=4,
         i1=2,
         i2=3,
         i3=5,
-        f=4,
     )
 
     final_pattern = "             b c i1 i3 f  "
     array_pattern = "             b c i1 i2    "
     index_pattern = "[i1, i2, i3] b         f r"
+    full_pattern = f"{final_pattern} <- {array_pattern}, {index_pattern} "
     array = generate_array(np, array_pattern=array_pattern, sizes=sizes)
     indexer = generate_indexer(np, index_pattern, sizes=sizes)
-    result = gather_scatter(f"{final_pattern} <- {array_pattern}, {index_pattern} ", array, indexer, i3=sizes["i3"])
-    indexer_as_dict = enumerate_indexer(numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
+    result = gather_scatter(full_pattern, array, indexer, i3=sizes["i3"])
+    indexer_as_dict = enumerate_indexer(_numpy_ixp, index_pattern, indexer=indexer, sizes=sizes)
 
     array_flat = flatten(np, array)
     result_flat = flatten(np, result)
@@ -357,6 +410,27 @@ def test_gather_scatter():
 
     assert np.max(abs(result_flat)) == 0
 
+    # check different aggregations
+    for agg_name, agg_func, default_value in list_aggname_aggfunc_default_value():
+        result_gst = gather_scatter(full_pattern, array, indexer, aggregation=agg_name, i3=sizes["i3"]).reshape(-1)
+        result_ref = np.full(
+            shape=[sizes[d] for d in final_pattern.split()], fill_value=default_value, dtype=array.dtype
+        ).reshape(-1)
+        for c in range(sizes["c"]):
+            flat_index_array = to_flat_index(array_pattern, {**indexer_as_dict, "c": c}, sizes=sizes)
+            flat_index_final = to_flat_index(final_pattern, {**indexer_as_dict, "c": c}, sizes=sizes)
 
-# TODO add tests for different aggregations in gather/scatter/gatherscatter
+            for ia, ir in zip(flat_index_array, flat_index_final, strict=True):
+                result_ref[ir] = agg_func(array_flat[ia], result_ref[ir])
+        assert np.allclose(result_ref, result_gst)
+
+    # check aggregation of constant
+    result_mean = gather_scatter(full_pattern, array * 0 + 3.0, indexer, aggregation="mean", i3=sizes["i3"])
+    result_sum = gather_scatter(full_pattern, array * 0 + 1, indexer, aggregation="sum", i3=sizes["i3"])
+    assert np.all(result_mean[result_sum > 0] == 3)
+    assert np.all(np.isnan(result_mean[result_sum == 0]))
+
+
 # TODO test gatherscatter with combination of scatter and gather
+# TODO test several patterns per combination?
+# TODO get a way to deal with indexers
